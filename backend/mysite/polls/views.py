dfb79d5f3c10300489c2f793d1345c01579ee790
SOUROV <sourovsarker005@gmail.com>
2022-12-01 17:31:11 +0600

    Namespacing URL names

--- backend/mysite/polls/views.py
+++ backend/mysite/polls/views.py
@@ -1,7 +1,7 @@
-
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.template import loader
 from django.template import loader
 
 from .models import Question
